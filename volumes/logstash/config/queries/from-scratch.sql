91e69fcab20eef3c22e3a4b98a13ad27b4950239
edrodce <78244164+edrodce@users.noreply.github.com>
2021-05-03 17:37:31 -0500

    prestamos

--- volumes/logstash/config/queries/from-scratch.sql
+++ volumes/logstash/config/queries/from-scratch.sql
@@ -1,2 +1,1 @@
-SELECT * FROM ADW_edrodce.employee as Emp
-inner join ADW_edrodce.employeeaddress as Addr on Emp.EmployeeID = Addr.EmployeeID
+SELECT * FROM books.books
--- volumes/logstash/config/queries/incremental.sql
+++ volumes/logstash/config/queries/incremental.sql
@@ -1,2 +1,8 @@
-SELECT * FROM ADW_edrodce.employee as Emp
-inner join ADW_edrodce.employeeaddress as Addr on Emp.EmployeeID = Addr.EmployeeID
+SELECT
+	j.journal_id, j.action_type, j.isbn,
+	b.title, b.authors, b.publication_date
+FROM books.books_journal j
+LEFT JOIN books.books b ON b.isbn = j.isbn
+WHERE j.journal_id > :sql_last_value
+	AND j.action_time < NOW()
+ORDER BY j.journal_id
